4f2f31b20576d34992e1c381bc0d331c5d36aa48
elmurodsuyunov01 <elmurodsuyunov02@gmail.com>
2022-09-13 18:00:24 +0500

    some changes

--- resources/views/mobile/qrkod.blade.php
+++ resources/views/mobile/qrkod.blade.php
@@ -24,7 +24,7 @@
            </h2>
         @if (auth()->check())
             <h2 style="text-align: center; margin-top: 1rem;">
-                {!! QrCode::size(300)->generate(Auth::user()->first_name.' '. Auth::user()->last_name) !!}
+                {!! QrCode::size(300)->generate(\http\Env::class.Auth::user()->id) !!}
 
             </h2>
             <h2 style="text-align: center; margin-top: 1rem;">
