cac4b2b7710fe5e5a3bb328ba2982a63a0e730a2
Muzaffar1908 <muzaffarahmadaliyev99@gmail.com>
2022-09-18 18:33:36 +0500

    Innoweek project

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>International Week of Innovative Ideas
+    <title>{{__('International Week of Innovative Ideas')}}
     </title>
     <!-- Favicons -->
     <link href="{{asset('frontend/image/favicon.ico')}}" rel="icon">
