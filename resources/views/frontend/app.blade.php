6cf4872cd7aec23dfdfaeff0356926bd2d309b22
Muzaffar1908 <muzaffarahmadaliyev99@gmail.com>
2022-09-19 11:01:58 +0500

    Innoweek project

--- resources/views/frontend/app.blade.php
+++ resources/views/frontend/app.blade.php
@@ -156,8 +156,8 @@
                     <a href="{{route('speakershowx', ['id'=>$spek->id])}}"><img src="{{asset('/upload/speaker/' . $spek->image.'-d.png')}}" alt="Speaker" width="267" height="267"></a>
                     </div>
                     <div class="content-box">
-                    <h3 class="title"><a href="{{route('speakershowx', ['id'=>$spek->id])}}">{!! substr($spek->{'full_name_'.App::getLocale()},0,90).'...' !!}</a></h3>
-                    <div class="sub-title">{!! substr($spek->{'description_'.App::getLocale()},0,90).'...' !!}</div>
+                    <h3 class="title"><a href="{{route('speakershowx', ['id'=>$spek->id])}}">{!!$spek->{'full_name_'.App::getLocale()}!!}</a></h3>
+                    <div class="sub-title">{!! ($spek->{'description_'.App::getLocale()}) !!}</div>
                     <div class="speaker-social">
                         <ul>
                         <li><a target="_blank" href="{{$spek->facebook_url}}"><i class="fab fa-facebook-f"></i></a></li>
