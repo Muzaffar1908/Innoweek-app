f37cbd9100961c2e79499b804f9f48af55b8dcd3
O'ktam <oktam.yoqubov@mail.ru>
2022-09-20 18:33:03 +0500

    xsss

--- resources/views/admin/layout/partials/head.blade.php
+++ resources/views/admin/layout/partials/head.blade.php
@@ -1,6 +1,7 @@
 
-<meta charset="UTF-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="">
 <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -56,4 +57,3 @@
 
 
 <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
-
