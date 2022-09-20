5eaebb05e4fa4ee4eca422b1821889b0afe8356b
Muzaffar1908 <muzaffarahmadaliyev99@gmail.com>
2022-09-20 19:18:01 +0500

    Innoweek project

--- resources/views/admin/layout/partials/head.blade.php
+++ resources/views/admin/layout/partials/head.blade.php
@@ -1,7 +1,4 @@
-
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="">
 <meta name="csrf-token" content="{{ csrf_token() }}">
