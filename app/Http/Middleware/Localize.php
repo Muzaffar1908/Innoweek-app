0b119a5034d1f7afb0bdc2aa3a77dc3b00f53915
O'ktam <oktam.yoqubov@mail.ru>
2022-10-14 19:36:56 +0500

    inno

--- app/Http/Middleware/Localize.php
+++ app/Http/Middleware/Localize.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class Localize
 {
@@ -18,6 +19,11 @@ class Localize
     {
         if (\Session::has('locale')) {
             \App::setlocale(\Session::get('locale'));
+            Carbon::setUTF8(true);
+            Carbon::setLocale(\App::currentLocale());
+            setlocale(LC_TIME, \App::currentLocale());
+            //dd(Carbon::getLocale());
+
         }
         return $next($request);
     }
